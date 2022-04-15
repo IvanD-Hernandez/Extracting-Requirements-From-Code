ba6e6127f930f1ebc0c7be49975cdb37e8b59791
Ivan Hernandez <ihernandez2018@my.fit.edu>
2022-04-15 19:19:25 -0400

    fixed small bug

--- src/Parse.cpp
+++ src/Parse.cpp
@@ -64,7 +64,7 @@ void Read_File(string file, File* Cur_File, LinkedList* Tree){
         }
         else{ // -----------------------------In a method------------------------------
 
-          if((temp.find('(') != -1) && (temp.find(')') != -1)
+          if((temp.find('(') != -1) && (temp.find(')') != -1) && (temp.find("return") == -1)
           && (temp.find(';') != -1) && (temp.find("for") == -1)&& (temp.find("while") == -1)){
             CurrentMethod = StripString(temp);
             if(MethodName == "main"){
